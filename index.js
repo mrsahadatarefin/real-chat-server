e8c747c00fb1aad24d4374feb65204915fcd39b3
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-03-21 23:52:37 +0600

    something new add

--- index.js
+++ index.js
@@ -23,10 +23,20 @@ io.on('connection',(socket)=>{
 socket.on('joined',({user})=>{
     users[socket.id]=user;
 console.log(`${user}has joined`)
+socket.broadcast.emit('userJoined',{user:'Admin',message:`${users[socket.id]} has joined`})
+socket.emit('welcome',{user:'Admin',message:` Welcome to the chat,${users[socket.id]}`})
 })
-socket.emit('welcome',{user:'Admin',message:` Welcome to the chat `})
+socket.on('message',({message,id})=>{
+    io.emit('sendMessage',{user:users[id],message,id})
+
+})
+
+socket.on('disconnected',()=>{
+    socket.broadcast.emit('leave',{user:'Admin',message:`${users[socket.id]} has left`})
+   console.log('user left') 
+})
  
-socket.broadcast.emit('userJoined',{user:'Admin',message:'user has joined'})
+
 })
 
 
